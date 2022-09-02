5432ebd33b9faab652b41b78aaaab1b6d209eff9
faazizpro <fahadafs11@gmail.com>
2022-09-02 19:27:14 +0600

    Dynamic API Data Load and search on keyboard enter button

--- js/phone.js
+++ js/phone.js
@@ -36,15 +36,16 @@ const displayPhones = (phones, dataLimit) =>{
 
 
     phones.forEach(phone => {
-        // console.log(phone);
+        console.log(phone);
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
         <div class="card p-4">
         <img src="${phone.image}" class="card-img-top w-75 d-block mx-auto m-3" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${phone.phone_name}</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
         </div>
       </div>
         `;
@@ -70,7 +71,16 @@ document.getElementById('btn-search').addEventListener('click', function (){
   // const searchText = searchField.value;
   // loadPhones(searchText);
 
-  processSearch(10)
+  processSearch(10);
+})
+
+
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function(e){
+  console.log(e.key);
+  if(e.key === 'Enter'){
+    processSearch(10)
+  }
 })
 
 
@@ -90,5 +100,13 @@ const toggleSpinner = isLoading => {
 document.getElementById('btn-show-all').addEventListener('click', function(){
   processSearch()
 })
+
+
+const loadPhoneDetails = id => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  fetch (url)
+  .then (res => res.json())
+  .then (data => console.log(data.data))
+}
 
 // loadPhones();
